74ae0fbce507e26ada1fb7e4eebda48deef0e51e
Corbin Enterline <corbinenterline1@gmail.com>
2021-01-07 12:39:53 -0600

    interview

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+Calculates the minimum number of operations to reach n.
+'''
+def minOperations(n):
+    '''
+    Copy pasta
+    '''
+    count = 0
+    bucket = 0
+    i = 1
+    if (n < i):
+        return 0
+    while (n > i):
+        if (i > bucket and n % i == 0):
+            bucket = i
+        else:
+            i += bucket
+        count += 1
+    return count
