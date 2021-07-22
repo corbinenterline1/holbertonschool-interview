3d8450afd03a8a0459f70a7071854bb0a211f045
Corbin Enterline <corbinenterline1@gmail.com>
2021-07-22 23:39:55 +0000

    queen plz

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -7,56 +7,40 @@ placing N non-attacking queens on an NN chessboard.
 import sys
 
 
-def valid_position(i, j):
-    """
-    Checks if position is valid
-    """
-    for q in range(0, N):
-        if board[i][q] == 1 or board[q][j] == 1:
-            return True
-    for q in range(0, N):
-        for diag in range(0, N):
-            if (q + diag == i + j) or (q - diag == i - j):
-                if board[q][diag] == 1:
-                    return True
-    return False
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+try:
+    size = int(sys.argv[1])
+except ValueError:
+    print("N must be a number")
+    exit(1)
+if size < 4:
+    print("N must be at least 4")
+    exit(1)
 
 
-def nqueen(n, start):
-    """
-    Solves the N queens problem. N >= 4
-    """
-    if n == 0:
-        return True
-    for i in range(0, N):
-        if i > 0:
-            start = 0
-        for j in range(start, N):
-            if (not(valid_position(i, j))) and (board[i][j] != 1):
-                board[i][j] = 1
-                if nqueen(n - 1, start):
-                    return True
-                board[i][j] = 0
-    return False
+def nqueens(size, y, queens):
+    """nqueens"""
+    for x in range(size):
+        hold = 0
+        for q in queens:
+            if x == q[1]:
+                hold = 1
+                break
+            if y - x == q[0] - q[1]:
+                hold = 1
+                break
+            if x - q[1] == q[0] - y:
+                hold = 1
+                break
+        if hold == 0:
+            queens.append([y, x])
+            if y != size - 1:
+                nqueens(size, y + 1, queens)
+            else:
+                print(queens)
+            del queens[-1]
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    try:
-        N = int(sys.argv[1])
-    except:
-        print("N must be a number")
-        exit(1)
-    if N < 4:
-        print("N must be at least 4")
-        exit(1)
-    for i in range(0, N):
-        board = [[0] * N for _ in range(N)]
-        nqueen(N, i)
-        solution = []
-        for row in range(0, N):
-            for col in range(0, N):
-                if board[row][col] == 1:
-                    solution.append([row, col])
-        if solution[0][1] == i:
+
+nqueens(size, 0, [])
