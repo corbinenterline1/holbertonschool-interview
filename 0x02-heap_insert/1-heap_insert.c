56f9394735733cff8d4c9a1e5a87d939affb6aff
Corbin Enterline <corbinenterline1@gmail.com>
2021-07-22 23:55:13 +0000

    heap insert

---
+++ 0x02-heap_insert/1-heap_insert.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+  * heap_insert - Inserts a value into a Max Binary Heap
+  * @root: root
+  * @value: value
+  *
+  * Return: new node
+  */
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+	heap_t *new_node;
+
+	if (root == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(heap_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = value;
+	if (*root == NULL)
+	{
+		*root = new_node;
+	}
+	return (new_node);
+}
