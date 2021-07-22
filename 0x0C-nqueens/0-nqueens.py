75be3573d4213d634fb3b03d0c2b17952738ade0
Corbin Enterline <corbinenterline1@gmail.com>
2021-07-22 23:42:48 +0000

    something wrong with my stuff

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -7,40 +7,25 @@ placing N non-attacking queens on an NN chessboard.
 import sys
 
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    exit(1)
-try:
-    size = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    exit(1)
-if size < 4:
-    print("N must be at least 4")
-    exit(1)
+def nqueens(n: int):
+    """
+    backtracking
+    """
+    matrix = [[0 for x in range(n)] for y in range(n)]
+    print(str(matrix))
 
 
-def nqueens(size, y, queens):
-    """nqueens"""
-    for x in range(size):
-        hold = 0
-        for q in queens:
-            if x == q[1]:
-                hold = 1
-                break
-            if y - x == q[0] - q[1]:
-                hold = 1
-                break
-            if x - q[1] == q[0] - y:
-                hold = 1
-                break
-        if hold == 0:
-            queens.append([y, x])
-            if y != size - 1:
-                nqueens(size, y + 1, queens)
-            else:
-                print(queens)
-            del queens[-1]
+if __name__ == "__main__":
+    if len(sys.argv) > 2 or len(sys.argv) < 2:
+        print("Usage: nqueens N")
+        exit(1)
 
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        exit(1)
 
-nqueens(size, 0, [])
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    nqueens(int(sys.argv[1]))
