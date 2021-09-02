f130904ceb019efcb64a0ee2901aa6e21af76353
Corbin Enterline <corbinenterline1@gmail.com>
2021-09-02 17:37:05 +0000

    0

---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int advanced_binary(int *array, size_t size, int value);
+int recursive_binary(int *array, int low, int high, int value);
+
+#endif
