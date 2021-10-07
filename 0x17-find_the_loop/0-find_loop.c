48a19ae83803dc39ee665acc0e645f9890b386a1
Corbin Enterline <corbinenterline1@gmail.com>
2021-10-07 11:18:32 +0000

    0x17, associated files

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - Finds the loop in a linked list.
+ * head: Head node of linked list.
+ * Return: Address of the node where loop starts, or NULL if no loop.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *turtle = head, *hare = head;
+
+	while (turtle && hare && hare->next)
+	{
+		turtle = turtle->next;
+		hare = hare->next->next;
+		if (turtle == hare)
+		{
+			turtle = head;
+			while (hare)
+			{
+				if (turtle == hare)
+					return (turtle);
+			
+			turtle = turtle->next;
+			hare = hare->next;
+			}
+		}
+	}
+	return (NULL);
+}
