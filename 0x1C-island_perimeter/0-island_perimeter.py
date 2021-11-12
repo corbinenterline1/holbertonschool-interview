1232db510711f943c2968c58dac898b0c84da0fa
Corbin Enterline <corbinenterline1@gmail.com>
2021-11-12 00:07:40 +0000

    Edge checks or

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -5,20 +5,22 @@ Island Perimeter Function for 0x1C - Island perimeter.
 
 
 def island_perimeter(grid):
-        """Checks grid array for 1's, then counts adjacent 0's in
-        cardinal directions.  Returns that count upon iterating through
-        array."""
+        """
+        Checks grid array for 1's (land), then counts adjacent 0's (water)
+        to that index with 1 in cardinal directions, clockwise.
+        Returns that count (perimeter) upon iterating through array.
+        """
 
         p = 0
         for r in range(len(grid)):
                 for c in range(len(grid)):
                         if grid[r][c] == 1:
-                                if grid[r - 1][c] == 0:
+                                if r == 0 or grid[r - 1][c] == 0:
                                         p += 1
-                                if grid[r][c + 1] == 0:
+                                if c == len(grid) - 1 or grid[r][c + 1] == 0:
                                         p += 1
-                                if grid[r + 1][c] == 0:
+                                if r == len(grid) - 1 or grid[r + 1][c] == 0:
                                         p += 1
-                                if grid[r][c - 1] == 0:
+                                if c == 0 or grid[r][c - 1] == 0:
                                         p += 1
         return p
