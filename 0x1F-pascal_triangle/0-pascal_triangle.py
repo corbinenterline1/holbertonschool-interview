851e8c9d2bfffa203453331d0f03a0b7d2e4027d
corbinenterline1 <corbinenterline1@gmail.com>
2022-01-06 20:06:03 -0600

    pep

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,8 @@
 """
 Pascal function
 """
+
+
 def pascal_triangle(n):
     """
     Creates a pascal triangle
@@ -10,7 +12,7 @@ def pascal_triangle(n):
     a = []
     new = []
     for line in range(1, n + 1):
-        C = 1 # used to represent C(line, i)
+        C = 1
         for i in range(1, line + 1):
             # Lines start with 1
             new.append(C)
