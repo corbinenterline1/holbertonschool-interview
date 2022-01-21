d05fc61d0251fb950e64ee91ec9ed121320e442c
corbinenterline1 <corbinenterline1@gmail.com>
2022-01-20 23:41:49 -0600

    0

---
+++ 0x22-primegame/0-prime_game.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""0x22 Prime Game"""
+
+
+def isWinner(x, nums):
+    """Prime game function."""
+    if not nums or x < 1:
+        return None
+    n = max(nums)
+    screen = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not screen[i]:
+            continue
+        for j in range(i * i, n + 1, i):
+            screen[j] = False
+    screen[0] = screen[1] = False
+    count = 0
+    for i in range(len(screen)):
+        if screen[i]:
+            count += 1
+        fltr[i] = count
+    p1 = 0
+    for n in nums:
+        p1 += screen[n] % 2 == 1
+    if p1 * 2 == len(nums):
+        return None
+    if p1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
---
+++ 0x22-primegame/README.md
@@ -0,0 +1,7 @@
+ # 0x22 - Interview - Prime Game
+Task # | Short Description
+-------|------------
+[Task 0](0-prime_game.py) | Prime game
+
+ ## Lessons Learned
+* Interview Prep
